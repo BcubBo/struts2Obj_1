024f517b8c2929eb439cfdbb7536fb1208d59a04
bowieu <fatalitywolf@foxmail.com>
2017-07-25 14:59:28 +0800

    添加图片

--- WebRoot/jsp/index3.jsp
+++ WebRoot/jsp/index3.jsp
@@ -1,12 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%String path = request.getContextPath(); %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Struts2 Demo</title>
 </head>
-<body>
+<body style="background-image:url('<%=path%>/image/3.gif');
+	background-repeat: true;width:auto;height:auto;">
 	<h1><strong>STRUTS2 START!</strong></h1>
 	<h1><strong>INDEX3.JSP</strong></h1>
 
--- src/action/DefaultAction.java
+++ src/action/DefaultAction.java
@@ -4,8 +4,30 @@ import com.opensymphony.xwork2.Action;
 
 public class DefaultAction {
 	
-
+	private String t;
 	
+	public String getT() {
+		return t;
+	}
+
+	public void setT(String t) {
+		this.t = t;
+	}
+////
+	
+	
+	//表示返回的具体名称
+	private String rs;
+	
+	
+	public String getRs() {
+		return rs;
+	}
+
+	public void setRe(String rs) {
+		this.rs = rs;
+	}
+
 	public String add() {
 		
 		
@@ -15,7 +37,18 @@ public class DefaultAction {
 	}
 
 	public String execute() throws Exception {
-		
+		if(t.equals("1")){
+			
+			rs="/jsp/index1.jsp";
+			
+			
+			
+		}else {
+			
+			rs="/jsp/index.jsp";
+			
+			
+		}
 		System.out.println("自创建的execute方法");
 		return "success";
 	}
