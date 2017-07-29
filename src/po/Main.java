61c649d9deb838b67ad3c54c298012fdbabff100
bowieu <fatalitywolf@foxmail.com>
2017-07-29 14:27:51 +0800

    抽风的进行了精简

--- src/po/Main.java
+++ src/po/Main.java
@@ -1,16 +1,11 @@
 /*package po;
 import po.TestMain;
 public class Main {
-	public void TestMainTest(){
-		String[] args = {"你好","周鸿顺"};
-		TestMain.main(args);
-		
-		
-	}
+
 	public static void main(String[]args) {
 		
-		
-		new Main().TestMainTest();
+		String[] args2 = {"你好","小伙子"};
+		TestMain.main(args2);
 		
 		
 	}
--- src/po/TestMain.java
+++ src/po/TestMain.java
@@ -1,15 +1,13 @@
 /*package po;
-
+import po.Main;
 public class TestMain {
 	public static void main(String[]args) {
 		
-		System.out.println("测试调用Main方法"+args[0]+"\t"+args[1]);
-
-
-		
+		System.out.println("测试调用Main方法"+
+				args[0]+
+				"\t"+args[1]);
+		Main.main(null);
 	}
-	
-	
 
 }
 */
