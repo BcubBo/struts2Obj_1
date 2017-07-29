79ecce0a77e8062d4912eba5229f7a910ef6edb3
bowieu <fatalitywolf@foxmail.com>
2017-07-29 10:03:51 +0800

    使用ognl表达式和struts2标签

---
+++ src/ognl/OgnlTest.java
@@ -0,0 +1,24 @@
+package ognl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OgnlTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+/*		fail("Not yet implemented");*/
+		
+		
+		
+		
+		
+	}
+
+}
