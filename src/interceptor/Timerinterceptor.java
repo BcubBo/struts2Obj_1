0ee25222735136148aab24a5fc49af573720fa43
bowieu <fatalitywolf@foxmail.com>
2017-08-05 15:42:11 +0800

    添加和配置interceptor,invaocation的iinvoke方法调用的位置很重要

---
+++ src/interceptor/Timerinterceptor.java
@@ -0,0 +1,31 @@
+package interceptor;
+
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+import po.User;
+
+public class Timerinterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// TODO Auto-generated method stub
+		Date begin = new Date();
+		Logger logger = (Logger)LogManager.getLogger();
+		String result = invocation.invoke();
+		//位置顺序很重要
+		Date end = new Date();
+
+		logger.debug(end.getTime());
+		logger.debug(begin.getTime());
+		logger.debug("interceptor进行了拦截总共method执行时间："+((end.getTime())-(begin.getTime())));
+		
+		return result;
+	}
+
+}
